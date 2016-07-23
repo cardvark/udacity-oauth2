ad1307fc56d1565ab3225912f4d1563cae9358e7
James Wang <jameswang@MacBook-Pro.local>
2016-07-23 16:30:31 -0700

    Added login links to header.html

--- project.py
+++ project.py
@@ -278,6 +278,9 @@ def restaurantsJSON():
 @app.route('/restaurant/')
 def showRestaurants():
     restaurants = session.query(Restaurant).order_by(asc(Restaurant.name))
+
+    if 'username' not in login_session:
+        return render_template('publicrestaurants.html', restaurants=restaurants)
     return render_template('restaurants.html', restaurants=restaurants)
 
 
